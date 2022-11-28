c08ff1d3d358ad9451835ff495e456979bc86ea7
jny <simone.vadilonga@helmholtz-berlin.de>
2022-11-28 11:50:29 +0100

    create a custom class that holds the raypyng-OphydDevices dictionary. Modify name of the elements to have a prefix, add docs.

--- src/raypyng_bluesky/RaypyngOphydDevices.py
+++ src/raypyng_bluesky/RaypyngOphydDevices.py
@@ -1,56 +1,110 @@
-from raypyng_bluesky.devices import SimulatedPGM, SimulatedApertures, SimulatedMirror, SimulatedSource
-from raypyng_bluesky.detector import RaypyngDetectorDevice, RaypyngTriggerDetector
-
-from raypyng_bluesky.run_engine_stuff import SupplementalDataRaypyng
+import os
+import sys
+import inspect
 
 from raypyng.rml import RMLFile
 
+from .devices import SimulatedPGM, SimulatedApertures, SimulatedMirror, SimulatedSource
+from .detector import RaypyngDetectorDevice, RaypyngTriggerDetector
+from .run_engine_stuff import SupplementalDataRaypyng
 
-import sys
 
+
+class RaypyngDictionary():
+    """A class defining a dictionary of the differen elements in rayui and the classe to be used as Ophyd devices
+    """    
+    
+
+    def __init__(self, *args,**kwargs):
+        self._mirrors = ['Toroid', 'PlaneMirror', 'Cylinder', 'Ellipsoid']
+        self._mirror_dict = {k:SimulatedMirror for k in self._mirrors}
+
+        self._sources = ['Dipole']
+        self._source_dict = {k:SimulatedSource for k in self._sources}
+
+        self._monos = ['PlaneGrating']
+        self._mono_dict = {k:SimulatedPGM for k in self._monos}
+
+        self._apertures = ['Slit']
+        self._aperture_dict = {k:SimulatedApertures for k in self._apertures}
+
+        self._detectors = ['ImagePlane']
+        self._detector_dict = {k:RaypyngDetectorDevice for k in self._detectors}
+
+        self._type_to_class_dict ={**self._mirror_dict, 
+                            **self._source_dict, 
+                            **self._mono_dict, 
+                            **self._aperture_dict,
+                            **self._detector_dict}
+        
+    @property
+    def type_to_class_dict(self):
+        return self._type_to_class_dict
+    
+    
+
+    
 class RaypyngOphydDevices():
+    """Create ophyd devices from a RAY-UI rml file and adds them to a name space.
 
-    def __init__(self, *args, RE, rml_path, temporary_folder, name_space=None, **kwargs):
+    If you are using ipython sys._getframe(0) returns the name space of the ipython instance.
+    (Remember to ``import sys``)
+
+    Args:
+        RE (RunEngine): Bluesky RunEngine
+        rml_path (str): the path to the rml file
+        temporary_folder (str): path where to create temporary folder
+        name_space (frame, optional): If None the class will try to understand the correct namespace to add the Ophyd devices to.
+                                    If the automatic retrieval fails, pass``sys._getframe(0)``. Defaults to None.
+        prefix (str): the prefix to prepend to the oe names found in the rml file
+    """    
+    def __init__(self, *args, RE, rml_path, temporary_folder, name_space=None, prefix=None, **kwargs):
+       
         
         self.RE = RE
 
         self.rml = RMLFile(rml_path)
-        self.temporary_folder = temporary_folder
-        self.name_space = name_space.f_globals
-        self.prefix = 'rp_'
+        self.temporary_folder = temporary_folder        
+        if name_space == None:
+            filename = inspect.stack()[1].filename
+            for i in inspect.stack():
+                if i.filename == filename:
+                    self.name_space = i.frame.f_globals
+        else:
+            self.name_space = name_space.f_globals
+            
+        if prefix == None:
+            self.prefix='rp_'
+        else:
+            self.prefix = prefix
+
+        rpg = RaypyngDictionary()
+        self.type_to_class_dict = rpg.type_to_class_dict
         
-
-        mirrors = ['Toroid', 'PlaneMirror', 'Cylinder', 'Ellipsoid']
-        mirror_dict = {k:SimulatedMirror for k in mirrors}
-
-        sources = ['Dipole']
-        source_dict = {k:SimulatedSource for k in sources}
-
-        monos = ['PlaneGrating']
-        mono_dict = {k:SimulatedPGM for k in monos}
-
-        apertures = ['Slit']
-        aperture_dict = {k:SimulatedApertures for k in apertures}
-
-        detectors = ['ImagePlane']
-        detector_dict = {k:RaypyngDetectorDevice for k in detectors}
-
-        self.type_to_class_dict ={**mirror_dict, 
-                            **source_dict, 
-                            **mono_dict, 
-                            **aperture_dict,
-                            **detector_dict}
+        self.prepend_to_oe_name()
         self.create_raypyng_elements_from_rml()
         self.create_trigger_detector()
         self.append_preprocessor()
     
+    def prepend_to_oe_name(self):
+        """Prepend a prefix to the name of all the Ophyd object created
+        """        
+        for oe in self.rml.beamline.children():
+            oe.attributes()['name']='rp_'+oe['name']
+
     def create_raypyng_elements_from_rml(self):
+        """Iterate through the raypyng objects created by RMLFile and create corresponding Ophyd Devices
+
+        Returns:
+            OphydDevices: the Ophyd devices created 
+        """        
         if self.name_space is None: 
+            # this does not work.. I get the wrong frame, the one of the class
             self.name_space = sys._getframe(1).f_globals
         ret = ()
         for oe in self.rml.beamline.children():
             cls = self.type_to_class_dict[oe['type']]
-            k = self.prefix + oe['name']
+            k = oe['name']
             if oe['type'] == 'ImagePlane':
                 self.name_space[k] = cls(name=k, rml=self.rml, tmp=self.temporary_folder)
                 ret = ret + (self.name_space[k],)
@@ -60,6 +114,8 @@ class RaypyngOphydDevices():
         return ret
     
     def create_trigger_detector(self):
+        """Create a trigger detector called RaypyngTriggerDetector
+        """        
         ret = ()
         k = 'TriggerDetector'
         cls = RaypyngTriggerDetector
@@ -67,9 +123,16 @@ class RaypyngOphydDevices():
         ret = ret + (self.name_space[k],)
 
     def trigger_detector(self):
+        """Return the RaypyngTriggerDetector
+
+        Returns:
+            RaypyngTriggerDetector (RaypyngTriggerDetector): the trigger detector
+        """        
         return self.name_space['TriggerDetector']
     
     def append_preprocessor(self):
+        """Add supplemental data to the RunEngine to trigger the simulations
+        """        
         TriggerDetector = self.trigger_detector()
         sd = SupplementalDataRaypyng(trigger_detector=TriggerDetector)
         self.RE.preprocessors.append(sd) 
